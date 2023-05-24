6ea236851b4aae9676dc53129b5f3fe17717fabf
Florencia Barrios <flopybarrios21@gmail.com>
2023-05-24 10:26:40 -0300

    cambiando el js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Definiendo precios a los medelos
 const preciosBase = {
   Ford: {
     Fiesta: 100,
@@ -36,14 +37,16 @@ const preciosBase = {
   }
 };
 
+//Cargar modelos de los autos asociadas a la marca del auto
 function cargarModelos() {
   const marca = document.getElementById("marca").value;
   const modelos = Object.keys(preciosBase[marca]);
 
+  //Generar las opciones de modelos dependiendo la eleccion de marca
   const modeloSelect = document.getElementById("modelo");
   modeloSelect.innerHTML = "<option value=''>Selecciona un modelo</option>";
 
-
+  //Bucle que recorre el array modelos
   for (let i = 0; i < modelos.length; i++) {
     const modelo = modelos[i];
     const option = document.createElement("option");
@@ -53,22 +56,23 @@ function cargarModelos() {
   }
 }
 
+//Calcular la cotización en base a lo pedido
 function calcularCotizacion() {
   const marca = document.getElementById("marca").value;
   const modelo = document.getElementById("modelo").value;
   const año = document.getElementById("año").value;
   const edad = document.getElementById("edad").value;
 
-
+  //Todos los datos deben ser rellenados
   if (!marca || !modelo || !año || !edad ) {
     alert("Por favor completa todos los campos");
     return;
   }
 
   const precioBase = preciosBase[marca][modelo];
-
   let coeficienteEdad = 1;
 
+  //Defieniendo el rango de edad
   if (edad >= 18 && edad < 25) {
     coeficienteEdad = 1.5;
   } else if (edad >= 25 && edad < 60) {
@@ -80,10 +84,15 @@ function calcularCotizacion() {
     return;
   }
   
-  let coeficienteAño = 1;
-
+  // Validar que el año no sea mayor al año actual
   const añoActual = new Date().getFullYear();
-  
+  if (año > añoActual) {
+    alert("El año ingresado no puede ser mayor al año actual");
+    return;
+  }
+
+  // Definir antigüedad del auto
+  let coeficienteAño = 1;
   const antiguedad = añoActual - año;
 
   if (antiguedad >= 0 && antiguedad < 5) {
@@ -97,6 +106,7 @@ function calcularCotizacion() {
     return;
   }
   
+  //Cotizacion final
   const precioCotizacion = precioBase * coeficienteEdad * coeficienteAño;
   const resultado = document.getElementById("resultado");
   resultado.textContent = `El precio de la cotización es: ${precioCotizacion}$`;
